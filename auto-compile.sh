634df1f9ee2773d84c1a570b017c21bbe695858f
JJ Wang <tozhuying@gmail.com>
2016-04-16 19:07:42 -0700

    add more subpages

--- auto-compile.sh
+++ auto-compile.sh
@@ -1,8 +1,8 @@
-when-changed src/template/florist_global.soy src/template/florist_home.soy \
+when-changed src/template/florist_global.soy src/template/florist_home.soy src/template/florist_catalog.soy src/template/florist_product.soy src/template/florist_cartpage.soy src/template/florist_payment.soy \
 -c \
 	java -jar bin/SoyToJsSrcCompiler.jar \
 			--outputPathFormat dist/js/'{INPUT_FILE_NAME_NO_EXT'}_generated.js \
-			--srcs src/template/florist_global.soy,src/template/florist_home.soy
+			--srcs src/template/florist_global.soy,src/template/florist_home.soy,src/template/florist_catalog.soy,src/template/florist_product.soy,src/template/florist_cartpage.soy,src/template/florist_payment.soy
 
 
 
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,78 @@
-goog.require('dom');
+// goog.require('goog.dom');
+
+// goog.events.listen(
+//     goog.dom.getElement('searchButton'),
+//     goog.events.EventType.CLICK,
+//     function(e) { alert("click") }
+// );
+
+var homeData = {
+  brand: "Fragrare",
+  cartItemList: [1,2],
+  marketingMsg: {
+    msg: "Show your love to mom",
+    savings: "20%",
+    holiday: "Mother's Day"
+  },
+  currentYear: new Date().getFullYear(),
+  productList:[{
+    imgURL: "static/imgs/arrangement-2_2.jpg",
+    price: 30,
+    name: "garden mix",
+    holiday: "Mother's Day"
+  },{
+    imgURL: "static/imgs/arrangement-3.jpeg",
+    price: 33,
+    name: "bright & cherry",
+    holiday: "Mother's Day"
+  },{
+    imgURL: "static/imgs/arrangement-6.jpeg",
+    price: 45,
+    name: "romantic mix",
+    holiday: "Anniversary"
+  },{
+    imgURL: "static/imgs/arrangement-5.jpeg",
+    price: 50,
+    name: "emma's collection",
+    holiday: "Mother's Day"
+  },{
+    imgURL: "static/imgs/arrangement-1.jpeg",
+    price: 38,
+    name: "four seasons",
+    holiday: "Anniversary"
+  },{
+    imgURL: "static/imgs/arrangement-10.jpeg",
+    price: 42,
+    name: "the blossom",
+    holiday: "Birthday's Day"
+  },{
+    imgURL: "static/imgs/arrangement-4.jpeg",
+    price: 30,
+    name: "pastel padals",
+    holiday: "Mother's Day"
+  },{
+    imgURL: "static/imgs/arrangement-11.jpeg",
+    price: 50,
+    name: "juicy",
+    holiday: "Birthday's Day"
+  }]
+};
+
+window.onload = function(){
+  document.getElementById("main-body").innerHTML = florist.home.home(homeData);
+
+  document.getElementById('searchButton').onclick = function (event){
+    event.preventDefault();
+    var catalogData = homeData;
+    catalogData['searchWord'] = document.getElementById("search_input").value;
+    document.getElementById("main-body").innerHTML = florist.catalog.page(catalogData);
+  };
+
+  // document.getElementsByClassName('product-box').onclick = function (event){
+  //   event.preventDefault();
+  //   var catalogData = homeData;
+  //   catalogData['searchWord'] = document.getElementById("search_input").value;
+  //   document.getElementById("main-body").innerHTML = florist.product.page(catalogData);
+  // };
+};
+
